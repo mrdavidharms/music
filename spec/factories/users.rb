2d93c04a9444edc586b29e724d2b92bdbeac21a4
David Harms <davidharms@MacBook-Pro.local>
2016-02-17 15:28:27 -0500

    first commit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:username) { |n| "User#{n}" }
+    sequence(:email) { |n| "person#{n}@gmail.com" }
+    password '12345678'
   end
-
 end
